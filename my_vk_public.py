f193391b74f4cbfd8cb63cea6e3b10a5f7e46d64
Anton Kiselev <straw.berry.pie@ya.ru>
2016-06-30 20:08:22 +0500

    Heroku compliance

--- my_vk_public.py
+++ my_vk_public.py
@@ -4,10 +4,11 @@ from os import environ
 
 # public_id = '-101534490'
 public_id = '-118729864' # mi_nerva
-vksession = vk.AuthSession(user_login=environ.get("VK_EMAIL"),
-                           user_password=environ.get("VK_PASS"),
-                           app_id='5244211',
-                           scope='wall,photos,offline')
+# vksession = vk.AuthSession(user_login=environ.get("VK_EMAIL"),
+#                            user_password=environ.get("VK_PASS"),
+#                            app_id='5244211',
+#                            scope='wall,photos,offline')
+vksession = vk.Session(access_token=environ.get("VK_ACCESS_TOKEN"))
 vkapi = vk.API(vksession, lang='ru')
 vkpublic = VKPublic(vkapi, public_id)
 

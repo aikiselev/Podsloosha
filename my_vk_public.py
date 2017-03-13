7e48abbeafe12ec5b16caf908d2148710d49770c
Anton Kiselev <straw.berry.pie@yandex.ru>
2017-03-13 13:04:41 +0300

    Small cleanup

--- my_vk_public.py
+++ my_vk_public.py
@@ -2,12 +2,7 @@ import vk
 from vk_public import VKPublic
 from os import environ
 
-# public_id = '-101534490' # posloosha.instagram : 119749591
 public_id = '-101534490' # posloosha.periscope
-# vksession = vk.AuthSession(user_login=environ.get("VK_EMAIL"),
-#                            user_password=environ.get("VK_PASS"),
-#                            app_id='5244211',
-#                            scope='wall,photos,offline')
 vksession = vk.Session(access_token=environ.get("VK_ACCESS_TOKEN"))
 vkapi = vk.API(vksession, lang='ru')
 vkpublic = VKPublic(vkapi, public_id)
